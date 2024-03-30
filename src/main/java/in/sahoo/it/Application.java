9b0372e05045214613c3b4d685e3331bae69b05f
soubhagya <soubhagyasahoo328@gmail.com>
2024-03-30 23:51:35 +0530

    added file successfully to git hub

--- src/main/java/in/sahoo/it/Application.java
+++ src/main/java/in/sahoo/it/Application.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+  priate map<String,object> cache=new HashMp<Strinhg,object>()
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		int i=1;
 	}
+	public void doCash()
+	{
+
 
 }
