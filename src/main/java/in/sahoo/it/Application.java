cd2ad47f6001b8d06512c263921e49c8fe21b23b
soubhagya <soubhagyasahoo328@gmail.com>
2024-03-30 23:27:16 +0530

    pushed to repository

--- src/main/java/in/sahoo/it/Application.java
+++ src/main/java/in/sahoo/it/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+private static final String REST_URL="http://www.equifax.com.getscores";
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		int i=1;
